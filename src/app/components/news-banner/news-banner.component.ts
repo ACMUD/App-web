b9d22e76c5711ba6d43f1726bd56e11bd1cb498b
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-11-28 23:42:43 -0500

    cambios para ngonchanges

--- src/app/components/news-banner/news-banner.component.ts
+++ src/app/components/news-banner/news-banner.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-news-banner',
   templateUrl: './news-banner.component.html',
   styleUrls: ['./news-banner.component.scss']
 })
-export class NewsBannerComponent implements OnInit {
+export class NewsBannerComponent implements OnInit, OnChanges {
   @Input() news: any;
+  FiveNews = [];
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    console.info("ngonchange")
+    // console.info(this.news)
+    this.FiveNews = this.news.slice(1,5);
+
+
   }
 
 }
