78db4bcdfdb754b0f62b03c3db42f992590997dc
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-08-04 00:35:27 -0500

    no import y hidden de componentes no usados y de news

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,20 +27,18 @@ import { FooterComponent } from './components/shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BannerComponent } from './pages/home/banner/banner.component';
 import { ContactoComponent } from './pages/home/contacto/contacto.component';
-import { TopnewsComponent } from './pages/home/topnews/topnews.component';
-import { NewsComponent } from './pages/news/news.component';
+// import { NewsComponent } from './pages/news/news.component';
 import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
-import { EventosComponent } from './pages/eventos/eventos.component';
+// import { EventosComponent } from './pages/eventos/eventos.component';
 import { GruposDeInteresComponent } from './pages/grupos-de-interes/grupos-de-interes.component';
 import { GrupoTarjetaComponent } from './pages/grupos-de-interes/grupo-tarjeta/grupo-tarjeta.component';
-import { TopnewsTarjetaComponent } from './pages/home/topnews/topnews-tarjeta/topnews-tarjeta.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NoticiaComponent } from './components/noticia/noticia.component';
-import { EventoComponent } from './components/evento/evento.component';
-import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+// import { NoticiaComponent } from './components/noticia/noticia.component';
+// import { EventoComponent } from './components/evento/evento.component';
+// import { ProyectosComponent } from './pages/proyectos/proyectos.component';
 import { IntegrantesComponent } from './pages/integrantes/integrantes.component';
-import { NewsBannerComponent } from './components/news-banner/news-banner.component';
-import { NewsCardComponent } from './components/news-card/news-card.component';
+// import { NewsBannerComponent } from './components/news-banner/news-banner.component';
+// import { NewsCardComponent } from './components/news-card/news-card.component';
 
 @NgModule({
   declarations: [
@@ -50,19 +48,17 @@ import { NewsCardComponent } from './components/news-card/news-card.component';
     HomeComponent,
     BannerComponent,
     ContactoComponent,
-    TopnewsComponent,
-    NewsComponent,
+    // NewsComponent,
     QuienesSomosComponent,
-    EventosComponent,
+    // EventosComponent,
     GruposDeInteresComponent,
     GrupoTarjetaComponent,
-    TopnewsTarjetaComponent,
-    NoticiaComponent,
-    EventoComponent,
-    ProyectosComponent,
+    // NoticiaComponent,
+    // EventoComponent,
+    // ProyectosComponent,
     IntegrantesComponent,
-    NewsBannerComponent,
-    NewsCardComponent
+    // NewsBannerComponent,
+    // NewsCardComponent
   ],
   imports: [
     BrowserModule,
